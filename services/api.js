8b877898753564eb01ee46d06fd84754843b9911
AliceGaz <gazizullina2010@yandex.ru>
2017-09-03 16:22:00 +0300

    api

--- services/api.js
+++ services/api.js
@@ -5,17 +5,33 @@
   function api($http, pathBase, NameParameterService) {
     queueApi.prototype = new baseApi($http);
     return {
-          getAllResponses: function(){
+          /*getAllResponses: function(){
             return $http.get('https://cc-survey-api.herokuapp.com/api/responses');
           },
           getAllQuestions: function(){
               return $http.get('https://cc-survey-api.herokuapp.com/api/questions')
+          },*/
+          getAllResponses: function(){
+            return $http({
+              url:'https://cc-survey-api.herokuapp.com/api/responses',
+              method:"GET",
+         });
+          },
+          getAllQuestions: function(){
+              return $http({
+                url:'https://cc-survey-api.herokuapp.com/api/questions',
+                method:"GET",
+           });
           },
           /*getOne: function(id){
             return $http.get('broker/questions'+id);
           },*/
           save: function(ancet){
-            return $http.post('broker/ancet-save', ancet)
+            return $http({
+              method  : "POST",
+              url     : 'https://cc-survey-api.herokuapp.com/api/questions',
+              data    : ancet,
+                        });
           },
           /*deleteOne: function(id)
           {
