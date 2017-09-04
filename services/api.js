ebca51cfac52bf8a0b7150956ae8a475ff3f0add
AliceGaz <gazizullina2010@yandex.ru>
2017-09-04 07:33:19 +0300

    fhsdgsdfg

--- services/api.js
+++ services/api.js
@@ -11,28 +11,12 @@
               return $http.get('https://cc-survey-api.herokuapp.com/api/questions')
           },*/
           getAllResponses: function(){
-            /*delete $http.defaults.headers.common['X-Requested-With'];
-            var r = $http.jsonp($sce.trustAsResourceUrl('https://cc-survey-api.herokuapp.com/api/responses'));
-            var l = $http({
-              method: 'GET',
-              url: $sce.trustAsResourceUrl('https://cc-survey-api.herokuapp.com/api/responses')
-            });*/
             return $http.get('https://cc-survey-api.herokuapp.com/api/responses');
           },
           getAllQuestions: function(){
-            /*delete $http.defaults.headers.common['X-Requested-With'];
-            var r = $http.jsonp($sce.trustAsResourceUrl('https://cc-survey-api.herokuapp.com/api/questions'));     
-            delete $http.defaults.headers.common['X-Requested-With'];
-            var l = $http({
-              method: 'GET',
-              url: $sce.trustAsResourceUrl('https://cc-survey-api.herokuapp.com/api/questions')
-            });*/    
               return $http.get('https://cc-survey-api.herokuapp.com/api/questions');
               
           },
-          /*getOne: function(id){
-            return $http.get('broker/questions'+id);
-          },*/
           save: function(ancet){
             return $http.post('https://cc-survey-api.herokuapp.com/api/questions', ancet);
           },
